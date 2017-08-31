c80816830de6e0fb6ea2830d576bfa14169c596b
kmsheng <kmsh3ng@gmail.com>
2017-08-31 08:33:35 +0800

    add another test case "How are you" for rule 1

--- test/index.js
+++ test/index.js
@@ -5,3 +5,6 @@ test('Rule 1 - "Good afternoon" in Tibetan', (t) => {
   t.is(getRootChar('ཕྱི་དྲོ་བདེ་ལེགས།'), 'ཕ');
 });
 
+test('Rule 1 - "How are you" in Tebetan', (t) => {
+  t.is(getRootChar('ཁྱེད་རང་སྐུ་གཇུགས་བདེ་པོ་ཡིན་པས།'), 'ཁ');
+});
