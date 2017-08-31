7410ab82ea16c070520735f77a29215fc5dfb3ad
kmsheng <kmsh3ng@gmail.com>
2017-08-31 08:54:39 +0800

    re-style

--- test/index.js
+++ test/index.js
@@ -1,15 +1,17 @@
 const test = require('ava');
 const {getRootChar} = require('./../src/');
 
-test('Rule 1 - "Good afternoon" in Tibetan', (t) => {
+test('Rule 1 - ཕྱི་དྲོ་བདེ་ལེགས། should have a root character ཕ', (t) => {
+  // "Good afternoon" in Tibetan
   t.is(getRootChar('ཕྱི་དྲོ་བདེ་ལེགས།'), 'ཕ');
 });
 
-test('Rule 1 - "How are you" in Tebetan', (t) => {
+test('Rule 1 - ཁྱེད་རང་སྐུ་གཇུགས་བདེ་པོ་ཡིན་པས། should have a root character ཁ', (t) => {
+  // "How are you" in Tibetan
   t.is(getRootChar('ཁྱེད་རང་སྐུ་གཇུགས་བདེ་པོ་ཡིན་པས།'), 'ཁ');
 });
 
-test('Rule 2 - ལྷ (\\u0f63\\u0f67) should have root character ཧ (\\u0f67)', (t) => {
+test('Rule 2 - ལྷ (\\u0f63\\u0f67) should have a root character ཧ (\\u0f67)', (t) => {
   t.is(getRootChar('ལྷ'), 'ཧ');
 });
 
