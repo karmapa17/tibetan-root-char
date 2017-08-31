762a817c99adf9e2589586ca86b35ba982dd97ce
kmsheng <kmsh3ng@gmail.com>
2017-08-31 08:34:16 +0800

    add test case for rule 2

--- test/index.js
+++ test/index.js
@@ -8,3 +8,7 @@ test('Rule 1 - "Good afternoon" in Tibetan', (t) => {
 test('Rule 1 - "How are you" in Tebetan', (t) => {
   t.is(getRootChar('ཁྱེད་རང་སྐུ་གཇུགས་བདེ་པོ་ཡིན་པས།'), 'ཁ');
 });
+
+test('Rule 2 - ལྷ (\\u0f63\\u0f67) should have root character ཧ (\\u0f67)', (t) => {
+  t.is(getRootChar('ལྷ'), 'ཧ');
+});
