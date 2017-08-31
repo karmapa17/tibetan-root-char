2819f17e5d2d4cd68466b444d9aa2c43203990bd
kmsheng <kmsh3ng@gmail.com>
2017-08-31 09:04:21 +0800

    update test case wording

--- test/index.js
+++ test/index.js
@@ -16,7 +16,7 @@ test('Rule 2 - ལྷ (\\u0f63\\u0f67) should have a root character ཧ (\\u0f67
   t.is(getRootChar('ལྷ'), 'ཧ');
 });
 
-test('Rule3 - Root character is before subscript like གླང contains a root character ག', (t) => {
+test('Rule 3 - Root character is before subscript like གླང contains a root character ག', (t) => {
   t.is(getRootChar('གླང'), 'ག');
 });
 
