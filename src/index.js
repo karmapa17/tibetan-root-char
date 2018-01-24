b652ac3488348b38aaffde5e5e5087580b5fd5c7
chiehan <chieh.an.hsu@gmail.com>
2018-01-24 13:16:27 +0800

    remove repeated regex syntax

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const rootCharMap = require('./rootCharMap');
 const tripleCharArr = require('./tripleCharArr');
 const sansCharMap = require('./sansCharMap');
 
-const sansFeature = /[ༀཊྚཋྛཌྜཎྞཥྵ\u0f43\u0f4d\u0f52\u0f57\u0f5c\u0f69\u0f93\u0f9d\u0fa2\u0fa7\u0fac\u0fb9ཱྰ\u0f73\u0f75-\u0f79\u0f7b\u0f7d-\u0f83ྶྺ]|ངྱ|གྶ|ནྡ|དྨ|ཙྪ|མྦ|[གཌདབཛ]ྷ|ཀྵ|ྒྷ|ྜྷ|ྡྷ|ྦྷ|ྫྷ|ྐྵ/;
+const sansFeature = /[ༀཊྚཋྛཌྜཎྞཥྵ\u0f43\u0f4d\u0f52\u0f57\u0f5c\u0f69\u0f93\u0f9d\u0fa2\u0fa7\u0fac\u0fb9ཱྰ\u0f73\u0f75-\u0f79\u0f7b\u0f7d-\u0f83ྶྺ]|ངྱ|གྶ|ནྡ|དྨ|ཙྪ|མྦ|[གཌདབཛ]ྷ|ྒྷ|ྡྷ|ྦྷ|ྫྷ/; // ྵ can find ཀྵ|ྐྵ ; ྜ can find ྜྷ
 
 function getTibetanRootChar(str) {
 
